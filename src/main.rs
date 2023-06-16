06b0ed861c2d45222c69d714fa27e60014be1f4c
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2023-06-17 01:20:41 +0800

    change `stats` main func

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod io;
 
 use clap::{Parser, Subcommand};
 use crate::logger::init_logger;
-use crate::stats::stats_all;
+use crate::stats::{stat_all_inputs};
 use crate::io::output_writer;
 
 fn main() {
@@ -22,13 +22,25 @@ fn main() {
     // println!("if rewrite: {}", cli.rewrite);
     match &cli.command {
         Commands::Stats { input } => {
-            stats_all(input, &mut writer);
+            // println!("input: {:?}", input);
+            // stats_all(input, &mut writer);
+            stat_all_inputs(input, &mut writer);
         }
     }
 }
 
 #[derive(Parser)]
-#[command(author, version, about = "seqkit in rust", long_about)]
+// #[command(author, version, about = "seqkit in rust", long_about)]
+#[command(name = "seqkit-rs")]
+#[command(about = "a cross-platform and ultrafast toolkit for FASTA/Q file manipulation")]
+#[command(long_about = "long_about todo!!!")]
+#[command(author, version)]
+#[command(
+    help_template =
+    "{name} -- {about}\n\nVersion: {version}\n\nAuthors: {author}\
+    \n\n{usage-heading} {usage}\n{all-args}"
+    ) // change template more!
+]
 struct Cli {
     /// Output file ("-" for stdout)
     #[arg(long, short, global = true, default_value = "-")]
@@ -37,7 +49,7 @@ struct Cli {
     #[arg(long, short, global = true, default_value = "false")]
     rewrite: bool,
     /// Subcommands
-    #[command(subcommand, name = "ww")]
+    #[command(subcommand)]
     command: Commands,
 
 }
@@ -47,9 +59,9 @@ enum Commands {
     /// Print statistics of the fastx file
     #[command(visible_alias = "stat")]
     Stats {
-        /// Input fastx file
-        #[arg(short, long, required = false)]
-        input: Option<String>,
+        /// Input FAST[A,Q] file
+        #[arg(required = false, global = true)]
+        input: Option<Vec<String>>,
     },
 
 }
